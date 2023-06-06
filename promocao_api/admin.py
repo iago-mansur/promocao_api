1345dc68de3e35f4a358b203b7720b69635bb496
iago-mansur <iagoma@gmail.com>
2023-06-06 16:45:10 -0300

    Finalizando modelo.

--- promocao_api/admin.py
+++ promocao_api/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Promocao
+from .models import Promocao, Empresa, Loja
 
-admin.site.register(Promocao)
+admin.site.register(Loja)
+admin.site.register(Empresa)
+
+@admin.register(Promocao)
+class PromocaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'empresa', 'loja')
--- promocao_api/models.py
+++ promocao_api/models.py
@@ -1,19 +1,35 @@
 from django.db import models
-
-class Promocao(models.Model):
-    nome = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.nome
     
 class Empresa(models.Model):
     nome = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Empresa'
+        verbose_plural = 'Empresas'
+
     def __str__(self):
         return self.nome
+
 
 class Loja(models.Model):
     nome = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Loja'
+        verbose_plural = 'Lojas'
+
     def __str__(self):
         return self.nome
+
+class Promocao(models.Model):
+    nome = models.CharField(max_length=200)
+    empresa = models.ForeignKey('Empresa', on_delete=models.CASCADE)
+    loja = models.ForeignKey('Loja', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Promoção'
+        verbose_plural = 'Promoções'
+
+    def __str__(self):
+        return f'{self.empresa}_{self.loja}: {self.nome}'
+"""'.promocao_api.Empresa'"""
