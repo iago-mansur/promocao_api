9c669908ab95581affbfe29fdf493a7428b14dda
iago-mansur <iagoma@gmail.com>
2023-06-06 14:29:23 -0300

    Incluindo modelo: loja, empresa.

--- promocao_api/models.py
+++ promocao_api/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class Promocao(models.Model):
-    empresa = models.CharField(max_length=200)
-    loja = models.CharField(max_length=200)
-    promocao = models.CharField(max_length=500)
+    nome = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.empresa + '_' + self.loja
+        return self.nome
+    
+class Empresa(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+class Loja(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
